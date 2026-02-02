bbe8f7107d4bd174124dc41d52c347efc72758d6
Anas <anaserkinjonov@gmail.com>
2026-02-02 17:16:37 +0500

    ci: update dev.yml

---
+++ ios/lib/screen/snip/SnipListScreen.swift
@@ -0,0 +1,14 @@
+//
+//  HomeScreen.swift
+//  ios
+//
+//  Created by Anas Erkinjonov on 30/01/26.
+//
+
+import SwiftUI
+
+struct SnipListScreen: View {
+    var body: some View {
+        Text("Snips Screen")
+    }
+}
